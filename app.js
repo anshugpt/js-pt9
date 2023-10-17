adc63309a5b3fad071fa662dd94bdb813f59c999
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-17 18:27:31 +0530

    added img file

---
+++ app.js
@@ -0,0 +1,7 @@
+//* DOM -> Document object 
+// the DOM represent a document with a logical tree
+// it allow us to manipulate / change webpage content (html elements)
+
+// by using javascript to change html, css is tow step prosses
+// 1. select
+//2. change / manipulate
